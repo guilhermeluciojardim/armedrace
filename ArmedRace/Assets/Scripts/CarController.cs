531b775c198d845c894637aae993b5e67fd21535
guilhermeluciojardim <guilhermelucio@gmail.com>
2022-08-16 00:26:28 -0300

    working on car physics

--- ArmedRace/Assets/Scripts/CarController.cs
+++ ArmedRace/Assets/Scripts/CarController.cs
@@ -17,18 +17,17 @@ public class CarController : MonoBehaviour
 
         public Axe1 axe1;
     }
-    public float maxAcceleration = 30.0f;
-    public float brakeAcceleration = 50.0f;
+    public float maxAcceleration;
+    public float brakeAcceleration;
 
-    public float turnSensitivity = 1.0f;
-    public float maxSteerAngle = 30.0f;
+    public float turnSensitivity;
+    public float maxSteerAngle;
 
     public Vector3 _centerOfMass;
 
     public List<Wheel> wheels;
 
     float moveInput, steerInput;
-    float incrementAcceleration = 15000;
     
 
     private Rigidbody carRb;
@@ -54,7 +53,9 @@ public class CarController : MonoBehaviour
     }
      void Move(){
         foreach(var wheel in wheels){
-            wheel.wheelCollider.motorTorque = moveInput * incrementAcceleration * maxAcceleration * Time.deltaTime;
+            if (wheel.axe1 == Axe1.Front){
+                wheel.wheelCollider.motorTorque = moveInput * maxAcceleration;
+            }
         }
     }
 
@@ -63,6 +64,7 @@ public class CarController : MonoBehaviour
             if (wheel.axe1 == Axe1.Front){
                 var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
                 wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+                
             }
         }
     }
@@ -80,7 +82,7 @@ public class CarController : MonoBehaviour
     void Brake(){
         if (Input.GetKey(KeyCode.Space)){
             foreach (var wheel in wheels){
-                wheel.wheelCollider.brakeTorque = incrementAcceleration/2 * brakeAcceleration * Time.deltaTime;
+                wheel.wheelCollider.brakeTorque = brakeAcceleration;
             }
         }
         else{
